ee98de47a595490f05125a057d0c3d9f0c2d02bd
Nnamdi Okeke <onnamdialex@gmail.com>
2022-06-30 23:18:59 +0100

    :construction: herobanner wip

---
+++ ecommerce/lib/client.js
@@ -0,0 +1,21 @@
+import SanityClient from "@sanity/client";
+import ImageUrlBuilder from "@sanity/image-url";
+
+export const client = SanityClient({
+  projectId: "xq9vji9e",
+
+  //dataset to know if we are in production or development
+  dataset: "production",
+
+  apiVersion: "2022-06-30",
+
+  useCdn: true,
+
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
+//to use the sanity images
+const builder = ImageUrlBuilder(client);
+
+//to get the urls for the images stored in sanity
+export const urlFor = (source) => builder.image(source);
--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { client } from "../lib/client";
 
 import { FooterBanner, HeroBanner, Product } from "../components";
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
       <HeroBanner />
@@ -11,11 +12,27 @@ const Home = () => {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  //to grab all the products from the sanity dashboard
+  const query = "*[__type == 'Product']";
+  //after grabbing the products, then :
+  const products = await client.fetch(query);
+
+  //to grab all the banners from the sanity dashboard
+  const bannerquery = "*[__type == 'banner']";
+  //after grabbing the banners, then :
+  const bannerData = await client.fetch(bannerquery);
+
+  return {
+    props: { products, bannerData },
+  };
 };
 
 export default Home;
