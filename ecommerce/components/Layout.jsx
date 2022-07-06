c40bb0b5799c31f99eb719c14d154a9e4b51432d
Nnamdi Okeke <onnamdialex@gmail.com>
2022-07-06 18:34:40 +0100

    :zap: layout component built out

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <div>Layout</div>
-  )
-}
+    <div className="layout">
+      <Head>
+        <title>Nemo's Storę</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
 
-export default Layout
+      <main className="main-container">{children}</main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
