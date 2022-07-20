ac28228582676e056a004926ec5d27a436d0e057
Nnamdi Okeke <onnamdialex@gmail.com>
2022-07-20 22:10:59 +0100

    :sparkles: stripe payment integration done!

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,8 +1,10 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log(req.body.cartItems);
+    console.log(req.body);
 
     try {
       // Create Checkout Sessions from body params.
@@ -15,19 +17,38 @@ export default async function handler(req, res) {
           { shipping_rate: "shr_1LNf2cKlJ5SR5J57YQXcAzeq" },
           { shipping_rate: "shr_1LNf58KlJ5SR5J573t9gBoEs" },
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: "{{PRICE_ID}}",
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img
+            .replace(
+              "image-",
+              "https://cdn.sanity.io/images/xq9vji9e/production/"
+            )
+            .replace("-webp", ".webp");
+          console.log("IMAGE", newImage);
+
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          };
+        }),
+
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       };
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
